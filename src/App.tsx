e82651716a5199450054d695dfc4f45f1f804313
Matheus Sixel <msixel22@gmail.com>
2025-07-22 20:25:51 -0300

    Criando componente Letter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 export default function App() {
   function handleRestartGame() {
@@ -13,6 +14,14 @@ export default function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame}/>
         <Tip tip="Uma das linguagens de programação mais utilizadas"/>
+        
+        <div className={styles.word}>
+        <Letter value="R" />
+        <Letter value="E" />
+        <Letter value="A" />
+        <Letter value="C" />
+        <Letter value="T" />
+        </div>
       </main>
     </div>
   )
